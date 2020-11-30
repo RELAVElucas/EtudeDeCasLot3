020dfd53ff3397533d67ef5ad5fa552dad1c6488
Myst-Click <guillaumecassoly@hotmail.fr>
2020-11-30 14:49:02 +0100

    create Models

---
+++ WebApplication1/Models/Client.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace WebApplication1.Models
+{
+    public class Client
+    {
+        [Required]
+        public int Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        public string FirstName { get; set; }
+        [Required]
+        public string Address { get; set; }
+    }
+}
---
+++ WebApplication1/Models/User.cs
@@ -0,0 +1,9 @@
+﻿using System;
+namespace WebApplication1.Models
+{
+    public class User
+    { 
+        public string Email { get; set; }
+        public string Password { get; set; }        
+    }
+}
