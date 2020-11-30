cedf79ddeba0e7c6f80a32e9eb362cc4ac490872
Myst-Click <guillaumecassoly@hotmail.fr>
2020-11-30 15:26:38 +0100

    controlleur Login

--- WebApplication1/Controllers/HomeController.cs
+++ WebApplication1/Controllers/HomeController.cs
@@ -3,16 +3,33 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using WebApplication1.Models;
 
 namespace WebApplication1.Controllers
 {
     public class HomeController : Controller
     {
+        public User user;
+
         public ActionResult Index()
         {
+            if(user == null)
+            {
+                return LoginView();
+            }
             return View();
         }
-
+        [HttpPost]
+        public ActionResult Login(User user)
+        {
+            //TO DO Check Login
+            this.user = user;
+            return this.Index();
+        }
+        public ActionResult LoginView()
+        {
+            return View("Login");
+        }
         public ActionResult About()
         {
             ViewBag.Message = "Your application description page.";
--- WebApplication1/Models/User.cs
+++ WebApplication1/Models/User.cs
@@ -1,9 +1,14 @@
 ﻿using System;
+using System.ComponentModel.DataAnnotations;
+
 namespace WebApplication1.Models
 {
     public class User
-    { 
+    {
+        [Required]
+        [EmailAddress]
         public string Email { get; set; }
+        [Required]
         public string Password { get; set; }        
     }
 }
